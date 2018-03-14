d5430b5d8ada91db6568d499fb9f186daaa2a8e8
Julie Fritsche <julie.fritsche@raphael-gunther.energy.local>
2018-03-14 15:02:12 +0100

    finished debugging react

--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -2,6 +2,11 @@ import React from 'react';
 import classes from './Person.css';
 
 const Person = (props) => {
+
+    /*const rnd = Math.random();
+    if (rnd > 0.7) {
+        throw new Error('Something went wrong');
+    }*/
     return (
         <div className={classes.person}>
             <p onClick={props.click}>I'm Person {props.name} and I am {props.age}!</p>
